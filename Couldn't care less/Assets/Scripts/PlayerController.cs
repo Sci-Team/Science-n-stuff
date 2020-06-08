5a518f1f190fc39e2b6e7f817f233784d28c6272
HRCooley <54008336+HRCooley@users.noreply.github.com>
2020-06-08 18:49:04 -0400

    Added Camera

---
+++ Couldn't care less/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float mouseY;
+    private float mouseX;
+
+    public float mouseSensitivity = 100f;
+    public GameObject gameCamera;
+    public GameObject playerCharacter;
+    public Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       rigidbody = GetComponent<Rigidbody>();
+
+       rigidbody.rotation = Quaternion.identity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+
+
+        ProccessCameraMovement();
+    }
+
+    void ProccessCameraMovement()
+    {
+        playerCharacter.transform.Rotate(0f, mouseX, 0f, Space.World);
+        gameCamera.transform.Rotate(-mouseY, 0f, 0f, Space.Self);
+    }
+}
