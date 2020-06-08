37fe2f695e0b607b89a62d072e21cdfe27f3dd81
HRCooley <54008336+HRCooley@users.noreply.github.com>
2020-06-08 19:00:32 -0400

    Update PlayerController.cs

--- Couldn't care less/Assets/Scripts/PlayerController.cs
+++ Couldn't care less/Assets/Scripts/PlayerController.cs
@@ -15,25 +15,23 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+       Cursor.lockState = CursorLockMode.Locked;
 
        rigidbody = GetComponent<Rigidbody>();
-
        rigidbody.rotation = Quaternion.identity;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //Takes input from mouse
         mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-
-
 
         ProccessCameraMovement();
     }
 
+    //uses mouse imput to rotated the camera and player model accordingly
     void ProccessCameraMovement()
     {
         playerCharacter.transform.Rotate(0f, mouseX, 0f, Space.World);
